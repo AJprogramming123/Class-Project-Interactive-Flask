d5dfa4d23ca883d3bc32fe87b2ad0a674677f8db
AJprogramming123 <jaimesandres2023@gmail.com>
2025-06-12 13:42:13 -0400

    finally done

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 '''
 This is where the main execution happens. I set it up to use imports so Python knows exactly where to look for things based on the path—it just makes the program easier to manage and read.
 
-From what I’ve learned, using a src folder is a cleaner way to organize the code. It helps keep things more expandable and straightforward, especially when using the __name__ == '__main__' trick. In my case, I went with App.__init__.py instead of just using src.__init__.py to make it a little more specific and modular.
+From what I’ve learned, using a src folder is a cleaner way to organize the code. It helps keep things more expandable and straightforward. In my case, I went with App.__init__.py instead of just using src.__init__.py to make it a little more specific and modular.
 
 I found out about simplifying Python project configuration by unifying package setups.
 - "https://realpython.com/python-pyproject-toml/"
